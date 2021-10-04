05815f02dfa3541f6504dc9fc4ecc0641e856ce0
Gustavo780904 <gustavodutrasantos@gmail.com>
2021-10-04 17:43:43 -0300

    projeto iniciado

---
+++ src/main/java/com/iftm/PostsMongDbApplication.java
@@ -0,0 +1,13 @@
+package com.iftm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsMongDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsMongDbApplication.class, args);
+	}
+
+}
